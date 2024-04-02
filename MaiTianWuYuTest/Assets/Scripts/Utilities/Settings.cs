5ec42a755683df6c4a117a6b9687bd647ec2a5a2
SamuelCloud <105068564+SamuelCloud@users.noreply.github.com>
2024-04-02 17:04:28 +0800

    DoTween插件，还有树木不一起渐隐的bug

--- MaiTianWuYuTest/Assets/Scripts/Utilities/Settings.cs
+++ MaiTianWuYuTest/Assets/Scripts/Utilities/Settings.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b8aaf4179599e5b4e5d8e4a3edd1d9146a913016db4e8651302abf5b0671ac1
-size 218
+oid sha256:94eaae6eee00a80e48135e989b9e89013bdd57c64e5567ff1925b1f4b67b3a3c
+size 252
